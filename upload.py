db0a14e0c793b345e2b4aa77225dbf1dd79f7859
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2016-09-06 16:33:16 +0530

    added folder and modified button name

--- upload.py
+++ upload.py
@@ -44,7 +44,7 @@ def upload_file():
     <h2>Upload new File</h2>
     <form action="" method=post enctype=multipart/form-data>
       <p><input type=file name=file>
-         <input type=submit value=Upload>
+         <input type=submit value=Calculate!>
     </form>
     '''
 
